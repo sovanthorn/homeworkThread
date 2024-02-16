c06cbde87bf42edea63ceb119bc735f692c712c7
Sovanthorn <nounsovannthon@gmail.com>
2024-02-16 19:40:51 +0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) {
-        try (FileWriter writer = new FileWriter("output.txt")) {
-            String text = "Hello, world!";
-            writer.write(text);
-            System.out.println("Data has been written to output.txt.");
-        } catch (IOException e) {
+        String messages =
+                "Welcome to CSTAD!\n"+
+                "********************************************************\n"+
+                "Don't give up this opportunity, do your best first\n"+
+                "---------------------------------------------------------\n"+
+                "Downloading...............completed 100%\n";
+
+        try {
+            for (int i = 0; i < messages.length(); i++) {
+                try {
+                    System.out.print(messages.charAt(i));
+                    Thread.sleep(100); // Adjust delay as needed
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
